8390482eee33288bf7bdcb2f29504a9a86c0d35b
Rena Hashimi <renhas54143@stud.noroff.no>
2025-02-17 01:21:20 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/layout';
+import HomePage from './Pages/HomePage';
+import VenuePage from './Pages/VenuePage';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
+import VenuesPage from './Pages/VenuesPage';
+import Profile from './Pages/Profile';
+import AboutUsPage from './Pages/AboutUs';
+import ContactPage from './Pages/Contact';
+import SingleProfilePage from './components/Profile/SingleProfilePage';
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="venues" element={<VenuesPage />} />
+        <Route path="venue/:id" element={<VenuePage />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="profiles/:name" element={<SingleProfilePage />} />
+        <Route path="about" element={<AboutUsPage />} />
+        <Route path="contact" element={<ContactPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
