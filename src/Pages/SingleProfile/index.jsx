e4d8fd1b157643ddcab978dba238e5f62ce42802
Rena Hashimi <renhas54143@stud.noroff.no>
2025-02-21 18:25:02 +0100

    Created SingleProfile

---
+++ src/Pages/SingleProfile/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SingleProfilePage from '../../components/Profile/SingleProfilePage';
+
+function SingleProfile() {
+  return (
+      <div>
+        <SingleProfilePage />
+      </div>
+  );
+}
+
+export default SingleProfile;
